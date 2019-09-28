96123f8a4bf4243c9a7ee7866da83a05c5cd83c7
jahnvi01 <jbdalwadi01@gmail.com>
2019-09-28 14:53:55 +0530

    xls to csv

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {OutTable, ExcelRenderer} from 'react-excel-renderer';
+import Dropzone from 'react-dropzone';
+import { CSVLink, CSVDownload } from "react-csv";
+var csvData=[];
+class App extends Component {
+  uploadfile(files){
+    console.log(files[0].path);
+  }
+  fileHandler = (event) => {
+    let fileObj = event.target.files[0];
 
-function App() {
-  return (
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+
+      console.log(fileObj);
+      if(err){
+        console.log(err);            
+      }
+      else{
+        console.log(resp.rows);
+        csvData=resp.rows;
+        this.setState({
+          cols: resp.cols,
+          rows: resp.rows
+        });
+      }
+    });               
+
+  }
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+ <div className="row" >
+     <div className="col-md-12">
+<form>
+<Dropzone onDrop={acceptedFiles => this.uploadfile(acceptedFiles)}>
+  {({getRootProps, getInputProps}) => (
+    <section>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </section>
+  )}
+</Dropzone>
+<br  />
+<input type="file" onChange={(event)=>{this.fileHandler(event)}} style={{"padding":"10px"}} />
+</form>
+<CSVLink data={csvData}>Download me</CSVLink>;
     </div>
-  );
+    </div>
+    </div>
+    );
+  }
 }
-
 export default App;
